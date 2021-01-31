bdb367922f1984cf9bdd631f8429df8f3daaeb01
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-01-31 22:23:08 +0530

    static files

--- Myapp/views.py
+++ Myapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
-class index(TemplateView):
-template_name = 'index.html'
+def index(request):
+    return render(request,"index.html")
